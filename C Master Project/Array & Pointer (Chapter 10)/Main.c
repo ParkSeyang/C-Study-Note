dd5b578d2fea0a723bfb90fdc0a04424cb8cea1c
ParkSeyang <ghlwkd14@naver.com>
2025-05-27 21:55:41 +0900

    Chapter 13 변수의영역과 데이터 공유

--- C Master Project/Array & Pointer (Chapter 10)/Main.c
+++ C Master Project/Array & Pointer (Chapter 10)/Main.c
@@ -389,17 +389,17 @@
 // 설명 : 1 ~ 45 중에 6개의 서로 다른 수를 배열에 입력하고 출력합니다. 입력한 수가 이미 저장된 수와 같으면  에러 메시지를 출력하고 다시입력합니다.
 // 다음 함수의 선언과 정의를 참고해 빈 부분을 채워 완성합니다.
 
-void input_nums(int* lotto_nums);  // 배열에 로또 번호를 입력하는 함수
-void print_nums(int* lotto_nums);  // 배열에 저장된 값을 출력하는 함수
-
-int main()
-{
-    int lotto_nums[6];  // 로또 번호를 저장할 배열
-
-    input_nums(lotto_nums);
-    print_nums(lotto_nums);
-    return 0;
-}
+// void input_nums(int* lotto_nums);  // 배열에 로또 번호를 입력하는 함수
+// void print_nums(int* lotto_nums);  // 배열에 저장된 값을 출력하는 함수
+// 
+// int main()
+// {
+//     int lotto_nums[6];  // 로또 번호를 저장할 배열
+// 
+//     input_nums(lotto_nums);
+//     print_nums(lotto_nums);
+//     return 0;
+// }
 
 // void input_nums(int* lotto_nums)
 // {
@@ -470,13 +470,13 @@ int main()
 // 
 // }
 
-void print_nums(int* lotto_nums)
-{
-    printf("로또 번호 : ");
-    for (int i = 0; i < 6; i++)
-    {
-        printf("%d ", lotto_nums[i]);
-    }
-}
+// void print_nums(int* lotto_nums)
+// {
+//     printf("로또 번호 : ");
+//     for (int i = 0; i < 6; i++)
+//     {
+//         printf("%d ", lotto_nums[i]);
+//     }
+// }
 
 #pragma endregion
